11fd1ce279bb810338bb05339bf70db7e05a9cca
Kyrylo Yakymenko <kya@combination.se>
2022-08-10 01:26:41 +0200

    Support flash cards in "flipped mode" - fixes #12

--- src/pages/[tenant]/flash/Flash.tsx
+++ src/pages/[tenant]/flash/Flash.tsx
@@ -6,6 +6,7 @@ export default function FlashCard() {
   const router = useRouter();
   const tenant = router.query.tenant as string;
 
+  const [isInFlippedMode, setIsInFlippedMode] = useState(false);
   const [isRevealed, setIsRevealed] = useState(false);
   const [previousWords, setPreviousWords] = useState<string[]>([]);
 
@@ -28,10 +29,23 @@ export default function FlashCard() {
 
   if (errorLoadingWord) return <div>Error!</div>;
 
+  const translationsList = word?.translations.map((t) => (
+    <div key={t.value}>{t.value}</div>
+  ));
+
+  const mainCardContents = isInFlippedMode ? translationsList : word?.value;
+  const revealedContents = isInFlippedMode ? word?.value : translationsList;
+
   return (
     <div>
       <h6 className="text-xl md:text-[1rem] leading-normal font-extrabold text-gray-700 flex justify-between">
         Flashcards for {tenant}
+        <button
+          className="border-black border-2"
+          onClick={() => setIsInFlippedMode((prevValue) => !prevValue)}
+        >
+          {isInFlippedMode ? "Flipped" : "Normal"}
+        </button>
       </h6>
       {isLoadingWord || isRefetchingWord || word === undefined ? (
         <div>Spinner...</div>
@@ -41,13 +55,9 @@ export default function FlashCard() {
             onClick={() => setIsRevealed((prevValue) => !prevValue)}
             className="border-2 border-blue-500 w-full h-80"
           >
-            <div className="text-6xl">{word.value}</div>
+            <div className="text-6xl">{mainCardContents}</div>
             <div className="flex flex-col">
-              {isRevealed
-                ? word.translations.map((t) => (
-                    <div key={t.value}>{t.value}</div>
-                  ))
-                : null}
+              {isRevealed ? revealedContents : null}
             </div>
           </button>
           <br />
