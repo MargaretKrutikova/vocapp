544dae63503a4fdc5380ec9547d12bfc9cf5e142
MargaretKrutikova <mkr@combination.se>
2022-08-11 19:25:17 +0200

    Pass uppercase env variables for atlas deploy

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -15,12 +15,12 @@ CONTAINER_NAME=tfstate
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
 export ARM_ACCESS_KEY=$ACCOUNT_KEY
 
-terraform plan --out=main.tfplan
-# terraform plan \
-#   -var "atlas_org_id=$atlas_org_id" \
-#   -var "mongodbatlas_private_key=$mongodbatlas_private_key" \
-#   -var "mongodbatlas_public_key=$mongodbatlas_public_key" \
-#   --out=main.tfplan
+# terraform plan --out=main.tfplan
+terraform plan \
+  -var "atlas_org_id=$ATLAS_ORG_ID" \
+  -var "mongodbatlas_private_key=$MONGODBATLAS_PRIVATE_KEY" \
+  -var "mongodbatlas_public_key=$MONGODBATLAS_PUBLIC_KEY" \
+  --out=main.tfplan
 
 terraform apply main.tfplan
 
