58587fa81e18b1dd6785862ed1c2feb8b214f6e0
Kyrylo Yakymenko <kya@combination.se>
2022-08-13 03:15:29 +0200

    Comment out cosmos, appinsights and webtest

--- infra/main.tf
+++ infra/main.tf
@@ -100,84 +100,84 @@ resource "azurerm_resource_group" "vocappgroup" {
   location = "westeurope"
 }
 
-resource "azurerm_cosmosdb_account" "cosmosaccount" {
-  name                 = "vocappcosmosaccount"
-  location             = azurerm_resource_group.vocappgroup.location
-  resource_group_name  = azurerm_resource_group.vocappgroup.name
-  kind                 = "MongoDB"
-  mongo_server_version = "4.0"
+# resource "azurerm_cosmosdb_account" "cosmosaccount" {
+#   name                 = "vocappcosmosaccount"
+#   location             = azurerm_resource_group.vocappgroup.location
+#   resource_group_name  = azurerm_resource_group.vocappgroup.name
+#   kind                 = "MongoDB"
+#   mongo_server_version = "4.0"
 
-  consistency_policy {
-    consistency_level = "Session"
-  }
+#   consistency_policy {
+#     consistency_level = "Session"
+#   }
 
-  capabilities {
-    name = "EnableServerless"
-  }
+#   capabilities {
+#     name = "EnableServerless"
+#   }
 
-  capabilities {
-    name = "EnableMongo"
-  }
+#   capabilities {
+#     name = "EnableMongo"
+#   }
 
-  geo_location {
-    location          = azurerm_resource_group.vocappgroup.location
-    failover_priority = 0
-  }
+#   geo_location {
+#     location          = azurerm_resource_group.vocappgroup.location
+#     failover_priority = 0
+#   }
 
-  backup {
-    type                = "Periodic"
-    interval_in_minutes = 1440
-    retention_in_hours  = 48
-  }
+#   backup {
+#     type                = "Periodic"
+#     interval_in_minutes = 1440
+#     retention_in_hours  = 48
+#   }
 
-  offer_type = "Standard"
-  # enable_free_tier = true
-}
+#   offer_type = "Standard"
+#   # enable_free_tier = true
+# }
 
-resource "azurerm_cosmosdb_mongo_database" "cosmosdb" {
-  name                = var.db_name
-  resource_group_name = azurerm_resource_group.vocappgroup.name
-  account_name        = azurerm_cosmosdb_account.cosmosaccount.name
-}
+# resource "azurerm_cosmosdb_mongo_database" "cosmosdb" {
+#   name                = var.db_name
+#   resource_group_name = azurerm_resource_group.vocappgroup.name
+#   account_name        = azurerm_cosmosdb_account.cosmosaccount.name
+# }
 
-resource "azurerm_cosmosdb_mongo_collection" "cosmoscollection" {
-  name                = var.collection_name
-  resource_group_name = azurerm_resource_group.vocappgroup.name
-  account_name        = azurerm_cosmosdb_account.cosmosaccount.name
-  database_name       = azurerm_cosmosdb_mongo_database.cosmosdb.name
+# resource "azurerm_cosmosdb_mongo_collection" "cosmoscollection" {
+#   name                = var.collection_name
+#   resource_group_name = azurerm_resource_group.vocappgroup.name
+#   account_name        = azurerm_cosmosdb_account.cosmosaccount.name
+#   database_name       = azurerm_cosmosdb_mongo_database.cosmosdb.name
 
-  index {
-    keys   = ["_id"]
-    unique = true
-  }
-}
+#   index {
+#     keys   = ["_id"]
+#     unique = true
+#   }
+# }
 
-resource "azurerm_application_insights" "vocappappinsights" {
-  name                = "vocapp-appinsights"
-  location            = azurerm_resource_group.vocappgroup.location
-  resource_group_name = azurerm_resource_group.vocappgroup.name
-  application_type    = "web"
-}
+# resource "azurerm_application_insights" "vocappappinsights" {
+#   name                = "vocapp-appinsights"
+#   location            = azurerm_resource_group.vocappgroup.location
+#   resource_group_name = azurerm_resource_group.vocappgroup.name
+#   application_type    = "web"
+# }
 
-resource "azurerm_application_insights_web_test" "vocappwebtest" {
-  name                    = "vocapp-appinsights-webtest"
-  location                = azurerm_resource_group.vocappgroup.location
-  resource_group_name     = azurerm_resource_group.vocappgroup.name
-  application_insights_id = azurerm_application_insights.vocappappinsights.id
-  kind                    = "ping"
-  frequency               = 300
-  timeout                 = 120
-  enabled                 = true
-  geo_locations           = ["emea-nl-ams-azr", "emea-gb-db3-azr"]
+# resource "azurerm_application_insights_web_test" "vocappwebtest" {
+#   name                    = "vocapp-appinsights-webtest"
+#   location                = azurerm_resource_group.vocappgroup.location
+#   resource_group_name     = azurerm_resource_group.vocappgroup.name
+#   application_insights_id = azurerm_application_insights.vocappappinsights.id
+#   kind                    = "ping"
+#   frequency               = 300
+#   timeout                 = 120
+#   enabled                 = true
+#   geo_locations           = ["emea-nl-ams-azr", "emea-gb-db3-azr"]
 
-  configuration = <<XML
-<WebTest Name="Health Check ping" Id="45E9A843-3A90-456C-A402-A44E94CA66C3" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="120" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
-  <Items>
-    <Request Method="GET" Guid="fbb64104-508e-4494-b994-2e1f4cd8c8fd" Version="1.1" Url="https://${var.webapp_name}.azurewebsites.net/api/healthcheck" ThinkTime="0" Timeout="120" ParseDependentRequests="False" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
-  </Items>
-</WebTest>
-  XML
-}
+#   configuration = <<XML
+# <WebTest Name="Health Check ping" Id="45E9A843-3A90-456C-A402-A44E94CA66C3" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="120" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
+#   <Items>
+#     <Request Method="GET" Guid="fbb64104-508e-4494-b994-2e1f4cd8c8fd" Version="1.1" Url="https://${var.webapp_name}.azurewebsites.net/api/healthcheck" ThinkTime="0" Timeout="120" ParseDependentRequests="False" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+#   </Items>
+# </WebTest>
+#   XML
+# }
 
 resource "azurerm_app_service_plan" "vocapp" {
   name                = "vocwebapp-plan"
@@ -192,9 +192,9 @@ resource "azurerm_app_service_plan" "vocapp" {
   }
 }
 
-locals {
-  con = split("?", azurerm_cosmosdb_account.cosmosaccount.connection_strings[0])
-}
+# locals {
+#   con = split("?", azurerm_cosmosdb_account.cosmosaccount.connection_strings[0])
+# }
 
 locals {
   con_parts = split("://", mongodbatlas_cluster.cluster-atlas.srv_address)
@@ -226,11 +226,11 @@ resource "azurerm_app_service" "vocappservice" {
   }
 }
 
-resource "mongodbatlas_project_ip_access_list" "appservice_atlas_ip" {
-  for_each = {
-    for key, value in azurerm_app_service.vocappservice.outbound_ip_address_list : key => value
-  }
-  project_id = mongodbatlas_project.atlas_project.id
-  ip_address = each.value
-  comment    = "App service IP address"
-}
+# resource "mongodbatlas_project_ip_access_list" "appservice_atlas_ip" {
+#   for_each = {
+#     for key, value in azurerm_app_service.vocappservice.outbound_ip_address_list : key => value
+#   }
+#   project_id = mongodbatlas_project.atlas_project.id
+#   ip_address = each.value
+#   comment    = "App service IP address"
+# }
