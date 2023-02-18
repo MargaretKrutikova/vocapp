6e5b4baf03a532d1b3dc6074570ae687e4b519a7
Kyrylo Yakymenko <kya@combination.se>
2023-02-18 19:12:07 +0100

    Revert shell script

--- local-dev/run-mongo.sh
+++ local-dev/run-mongo.sh
@@ -1,2 +1,2 @@
 docker-compose up -d
-docker-compose exec mongo mongo --eval "rs.initiate({_id: 'rs0', members: [{_id: 0, host: 'localhost:27017'}]});"
+# docker-compose exec mongo mongo --eval "rs.initiate({_id: 'rs0', members: [{_id: 0, host: 'localhost:27017'}]});"
